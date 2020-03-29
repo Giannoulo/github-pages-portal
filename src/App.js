831c2bbb480130462fd36b1370a4ed251cb14ca9
Giannoulo <ggiannoulopoulos@konnektable.com>
2020-03-29 19:15:25 +0300

    removed image

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Portal Incoming
+          Despoina Gamiesai
         </a>
       </header>
     </div>
