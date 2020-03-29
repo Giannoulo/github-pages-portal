3f36f0da86eca2adb92fe77b82589281b7f1eb87
Giannoulo <ggiannoulopoulos@konnektable.com>
2020-03-29 16:38:16 +0300

    Updates

---
+++ precache-manifest.547eb5eb8ede5d1bc957b444a01b72bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc28fd2d3ebd514d6a7d9c52f8ba164",
+    "url": "/github-pages-portal/index.html"
+  },
+  {
+    "revision": "132e3d0978b1690ae8b7",
+    "url": "/github-pages-portal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f3021b7ef5dcfbcdb3e0",
+    "url": "/github-pages-portal/static/js/2.1b79a414.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-portal/static/js/2.1b79a414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132e3d0978b1690ae8b7",
+    "url": "/github-pages-portal/static/js/main.9d431ee7.chunk.js"
+  },
+  {
+    "revision": "739094e03b23eff9aca5",
+    "url": "/github-pages-portal/static/js/runtime-main.c2faa9a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
